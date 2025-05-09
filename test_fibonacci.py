7ec52a135ea1471fe0dddd1d4ec3e5fa6ef0ccde
JoseAngelMarquez <126813977+JoseAngelMarquez@users.noreply.github.com>
2025-05-10 01:34:13 +0200

    Update test_fibonacci.py

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -5,9 +5,11 @@ def test_fibonacci():
     assert fibonacci(0) == 0
     assert fibonacci(1) == 1
     assert fibonacci(5) == 5
-    print("¡Pruebas pasadas!")
+    print("Pruebas pasadas ! Codigo de Jose")
 
 if __name__ == "__main__":
     test_fibonacci()
-    time = timeit.timeit("fibonacci(20)", setup="from fibonacci import fibonacci", number=100)
-    print(f"Tiempo: {time:.4f} segundos")
+    time = timeit.timeit("fibonacci(20)", 
+                         setup="from fibonacci import fibonacci", 
+                         number=100)
+    print(f"Tiempo de ejecucion (100 iteraciones de fibonacci (20) ) : {time:.4f} segundos")
